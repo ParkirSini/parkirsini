acde9602a8039923ad49258995b44baaa0c01956
Convinceo8 <dual.haseo@gmail.com>
2023-06-30 18:50:07 +0700

    added folders, helpers, and simple routing

---
+++ app.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const router = require("./routes");
+var cors = require("cors");
+
+app.use(cors());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(router);
+
+module.exports = app;
