1a575c2360e276b87f786ce42f98ef9d30fff384
Convinceo8 <dual.haseo@gmail.com>
2023-07-01 19:06:05 +0700

    feat:added some endPoints to crud facility and also to ligin and register

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,12 @@
 const router = require("express").Router();
 const Controller = require("../controllers/index");
+const errorHandler = require("../middlewares/errorHandler");
+const customerRouter = require("./customer")
+const landlordRouter = require("./landlord")
 
 router.get("/", Controller.home);
+router.use("/pub", customerRouter)
+router.use("/owner", landlordRouter)
+router.use(errorHandler);
 
 module.exports = router;
