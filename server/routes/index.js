0e397f6d26710030d2e3d3473993cfac6a082196
Convinceo8 <dual.haseo@gmail.com>
2023-07-01 19:24:28 +0700

    added login and register

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,10 +4,15 @@ const ControllerImage = require("../controllers/ImageUpload");
 const multer = require('multer')
 const { storage, cloudinary } = require("../cloudinary")
 const upload = multer({ storage })
+const errorHandler = require("../middlewares/errorHandler");
+const customerRouter = require("./customer")
+const landlordRouter = require("./landlord")
 
 router
    .get("/", Controller.home)
-   .post("/image", upload.array('image'), ControllerImage.upload);
-
+   .post("/image", upload.array('image'), ControllerImage.upload)
+   .use("/pub", customerRouter)
+   .use("/owner", landlordRouter)
+   .use(errorHandler);
 
 module.exports = router;
