f4c4442cc68e0da8815aaf8e9ba03d58f058535e
Einszweidre <hangga95@gmail.com>
2023-07-01 10:58:26 +0700

    feat: modify hash to 10

--- server/helpers/bcrypt.js
+++ server/helpers/bcrypt.js
@@ -1,6 +1,6 @@
-const bcrypt = require("bcryptjs")
+const bcrypt = require("bcryptjs");
 
-const hashPassword = (pass) => bcrypt.hashSync(pass, 8)
-const compareHash = (pass, hashed) => bcrypt.compareSync(pass, hashed)
+const hashPassword = (pass) => bcrypt.hashSync(pass, 10);
+const compareHash = (pass, hashed) => bcrypt.compareSync(pass, hashed);
 
-module.exports = { hashPassword, compareHash }
+module.exports = { hashPassword, compareHash };
