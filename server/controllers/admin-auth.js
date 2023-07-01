fb9fc74d8f9523b2828400adee2c8e6fda72e8e5
Einszweidre <hangga95@gmail.com>
2023-07-01 11:00:06 +0700

    feat: add login and register admin

---
+++ server/controllers/admin-auth.js
@@ -0,0 +1,54 @@
+const { Landlord } = require("../models");
+const { compareHash } = require("../helpers/bcrypt");
+
+async function processLogin(email, password) {
+  const findUser = await Landlord.findOne({
+    where: {
+      email,
+    },
+  });
+  if (!findUser) throw { name: "DataNotFound" };
+  const checkPassword = compareHash(password, findUser.password);
+  if (!checkPassword) throw { name: "InvalidUser" };
+  const access_token = encodeToken({
+    id: findUser.id,
+    email: findUser.email,
+  });
+
+  return access_token;
+}
+
+class Auth {
+  static async login(req, res, next) {
+    try {
+      const { email, password } = req.body;
+      const access_token = await processLogin(email, password);
+      res.json({ access_token });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async register(req, res, next) {
+    try {
+      const { email, password, username, phoneNumber, address } = req.body;
+      const createUser = await Landlord.create({
+        email,
+        password,
+        username,
+        role: "Admin",
+        phoneNumber,
+        address,
+        amount: 0,
+      });
+
+      res.status(201).json({
+        message: `Create user admin ${createUser.email} success`,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+module.exports = Auth;
